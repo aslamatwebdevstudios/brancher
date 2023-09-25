98656102cbf2c4006fe0eecc2c711ccb6c9c97c9
Aslam Doctor <aslam.doctor@gmail.com>
2023-09-25 19:27:32 +0530

    Add enhancements
    - convert branch name to lowercase
    - add feature to copy to clipboard automatically on clicking the branch name

--- content.js
+++ content.js
@@ -7,6 +7,17 @@ const slugify = (str) =>
     .replace(/[\s_-]+/g, "-")
     .replace(/^-+|-+$/g, "");
 
+const copyToClipboard = ( copyText ) => {
+  navigator.clipboard.writeText(copyText).then(() => {
+    //clipboard successfully set
+    alert("Copied branch name: " + copyText);
+  }, () => {
+      //clipboard write failed, use fallback
+  });
+
+}
+
+
 setTimeout(() => {
   const tiketTitle = document.querySelector(
     "*[data-test-id='issue.views.issue-base.foundation.summary.heading']"
@@ -34,19 +45,14 @@ setTimeout(() => {
       div.id = "brancher-content";
       target.appendChild(div);
       if (tiketTitle) {
-        title = `ticket/${ticket}-${slugify(tiketTitle.textContent)}`;
-        div.innerHTML = title;
+        title = `ticket/${ticket}-${slugify(tiketTitle.textContent)}`.toLocaleLowerCase();
+        div.innerHTML = `<span id="branch-name-text" style="display:inline-block; user-select:all; background-color:#ccc; padding:5px; border-radius:5px;color: #000; margin-left: 20px;">${title}</span>`;
       }
     }
-
-    
   }
 
-  
+  var branchNameText = document.querySelector('#branch-name-text');
+  branchNameText.addEventListener('click', function() {
+    copyToClipboard(branchNameText.innerText);
+  });
 },3000);
-
-
-
-
-
-
